f35a1bc089888bbebc82ea0cae69138d7005a7ab
gettinToasty <sean.r.beyer@gmail.com>
2017-01-03 15:57:37 -0800

    Add dynamic searching by tag

--- frontend/components/todo_list/todo_list.jsx
+++ frontend/components/todo_list/todo_list.jsx
@@ -5,18 +5,48 @@ import TodoForm from './todo_form';
 class TodoList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {search:"", todos: this.props.todos};
+    this.updateSearch = this.updateSearch.bind(this);
+    this.filterTodos = this.filterTodos.bind(this);
   }
 
   componentDidMount() {
     this.props.fetchTodos();
   }
 
+  componentWillReceiveProps(nextProps) {
+    this.setState({ todos: nextProps.todos });
+  }
+
+  updateSearch(e) {
+    this.setState(
+      {search: e.target.value},
+      this.filterTodos
+    );
+  }
+
+  filterTodos() {
+    let newTodos = [];
+    this.props.todos.forEach(todo => {
+      if (todo.tags.some(tag => {
+        return tag.name.includes(this.state.search);
+      })) { newTodos.push(todo); }
+    });
+    if (this.state.search === "") {
+      newTodos = this.props.todos;
+    }
+    this.setState({todos: newTodos});
+  }
+
   render() {
     return (
       <div>
         <h1>Todo:</h1>
+        <label>Search by Tag:
+          <input onChange={this.updateSearch} value={this.state.search}></input>
+        </label>
         <ul>
-          {this.props.todos.map((todo) => {
+          {this.state.todos.map((todo) => {
             return <TodoListItem todo={todo}
                                  key={todo.id}
                                  updateTodo={this.props.updateTodo}/>;
